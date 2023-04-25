b3bf95283f93a532c24d999b67a786a4809963ed
kajuraedrwin <kajuraedrwin@gmail.com>
2023-04-25 14:48:13 -0700

    Add _printf.c file

---
+++ _printf.c
@@ -0,0 +1,109 @@
+#include <stdarg.h>
+
+/**
+ * _printf - takes in variable number of aguments
+ * @*format: pointer to the first address of the variable arguments
+ * @... : infinite variable list
+ * Return: counter
+ */
+
+int _printf(const char *format, ...)
+{
+	va_list args;
+
+	va_start(args, format);
+
+	int i = 0;
+
+	int counter = 0;
+
+	while (format[i])
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			switch (format[i])
+			{
+				case 'd':
+					{
+						int x = va_arg(args, int);
+
+						printf("%d", x);
+						break;
+					}
+				case 'f':
+					{
+						 double x = va_arg(args, double);
+
+						 printf("%f", x);
+						 break;
+					}
+				case 'c':
+					{
+						int x = va_arg(args, int);
+
+						printf("%c", x);
+						break;
+					}
+				case 'i':
+					{
+						int x = va_arg(args, int);
+
+						printf("%d", x);
+						break;
+					}
+				case 's':
+					{
+						char *x = va_arg(args, char*);
+
+						printf("%s", x);
+						break;
+					}
+				case 'x':
+				case 'X':
+					{
+						int x = va_arg(args, int);
+
+						printf("%x", x);
+						break;
+					}
+				case 'p':
+					{
+						void *x = va_arg(args, void*);
+
+						printf("%p", x);
+						break;
+					}
+				case 'o':
+					{
+						int x = va_arg(args, int);
+
+						printf("%o", x);
+						break;
+					}
+				case 'u':
+					{
+						int x = va_arg(args, int);
+
+						printf("%u", x);
+						break;
+					}
+				case '%':
+					{
+						putchar('%');
+						break;
+					}
+				default:
+					printf("%%%c", format[i]);
+					break;
+			}
+		} else
+		{
+			putchar(format[i]);
+		}
+		i++;
+		counter++;
+	}
+	va_end(args);
+	return (counter);
+}
